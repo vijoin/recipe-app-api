2950746d8b2efef948165528656cc10e037cf8cd
Victor Inojosa <vijoin@gmail.com>
2020-05-31 16:49:38 -0300

    Fix flake8 lint

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -15,9 +15,9 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='test@vijoin.com',
             password='password123',
-            name='Test user fullname' 
+            name='Test user fullname'
         )
-    
+
     def test_users_listed(self):
         """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
@@ -33,7 +33,7 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-    
+
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
