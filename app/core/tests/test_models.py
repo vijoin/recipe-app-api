8b2810ea85ee1362e1caf7f468bd978d003043f1
Victor Inojosa <vijoin@gmail.com>
2020-05-31 13:45:59 -0300

    Add custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@vijoin.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_email_normalized(self):
+        """Test creating a new user with an capitalizaed email is normalized"""
+        email = 'test@VIJOIN.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+        )
+        
+        self.assertEqual(user.email, email.lower())
+    
+    def test_create_user_without_email_raise_error(self):
+        """Test create a new user without email raises a Value Error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@vijoin.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
